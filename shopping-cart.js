2a00f207139dd56ebdf422b60f6f13953bff7087
kasperken <kennethallanandrews@gmail.com>
2023-04-17 03:00:09 -0400

    Made ui nicer and streamlined things. Added ability to select which card user chooses to use.

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,9 +1,18 @@
+const prompt = require('prompt-sync')();
+console.clear()
+
 const shoppingCart = [
   { name: 'Apple', price: 1.50, quantity: 6 },
     { name: 'Pie', price: 8, quantity: 1 },
     { name: 'Magazine', price: 2, quantity: 1 },
 ]
 
+function displayCart(cart) {
+    for (let i = 0; i <= cart.length - 1; i++) {
+        console.log(cart[i].name + ': $' + cart[i].price + 'ea - Count: ' + cart[i].quantity + ' - Cost: $' + (cart[i].price * cart[i].quantity));
+    }
+    console.log("\n                  Total: $" + addTotal(cart) + '\n');
+}
 
 function addToCart(item, price, quantity) {
     shoppingCart.push({ name: item, price: price, quantity: quantity });
@@ -29,35 +38,50 @@ function addTotal(cart) {
 }
 
 creditCards = [
-    { name: 'Visa', ammount: 10 },
-    { name: 'Master Card', ammount: 0},
-    { name: 'Discover Card', ammount: 100 },
+    { name: 'Visa', balance: 10 },
+    { name: 'Master Card', balance: 0},
+    { name: 'Discover Card', balance: 100 },
 ]
 
-let totalPrice = addTotal(shoppingCart);
-
-function processPayment(creditCard, remainingTotal) {
-    if (creditCard.ammount > remainingTotal) {
-        creditCard.ammount -= remainingTotal;
-        remainingTotal -= remainingTotal;
+function selectCreditCard() {
+    console.log('\nPlease Select a Payment Method:');
+    for (let i = 0; i <= creditCards.length - 1; i++) {
+        console.log(i + '. ' + creditCards[i].name + ' - $' + creditCards[i].balance);
     }
-      else {
-        remainingTotal -= creditCard.ammount;
-        creditCard.ammount -= creditCard.ammount;
-      }
+    let creditCardSelection = prompt('Select a Credit Card: ');
+    creditCardSelection = creditCards[creditCardSelection];
+    return creditCardSelection;
+}
 
+function processPayment(remainingTotal) {
+    console.log('----------------------------------------------------');
+    creditCard = selectCreditCard();
+    console.clear()
+    console.log('...processing Payment with ' + creditCard.name + '.\n');
+    console.log('Previous Total Due: $' + remainingTotal);
+    let paymentAmount = 0;
+    if (creditCard.balance > remainingTotal) {
+        paymentAmmount = remainingTotal;
+    }  else {
+        paymentAmmount = creditCard.balance;
+      }
+    creditCard.balance -= paymentAmmount;
+    remainingTotal -= paymentAmmount;
+    console.log('payment amount: $' + paymentAmount);
+    console.log('\n             Total Due: $'  + remainingTotal);
+    console.log('----------------------------------------------------');
+    console.log('\nYour Remaining ' + creditCard.name + ' Balance is $' + creditCard.balance +'.\n');
     return remainingTotal;
 }
 
-function checkout(creditCard, totalPrice) {
-    for (let i = 0; i <= creditCards.length - 1; i++) {
-        if (totalPrice > 0) {
-            totalPrice = processPayment(creditCards[i], totalPrice);
-        }
+function checkout(cart) {
+    let remainingTotal = addTotal(cart);
+    while (remainingTotal > 0 ) {
+        remainingTotal = processPayment(remainingTotal);
     }
+    console.log('----------------------------------------------------');
+    console.log('Transaction Successful! Thank you for shopping with us!\n\n\n');
 }
 
-console.log(shoppingCart);
-console.log(addTotal(shoppingCart));
-checkout(creditCards, totalPrice);
-console.log(creditCards);
+displayCart(shoppingCart);
+checkout(shoppingCart);
